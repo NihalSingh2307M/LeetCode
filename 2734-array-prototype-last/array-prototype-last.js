754610195203f97dbdb21b76172e16604be2e335
NihalSingh2307M <nihalsingh2307n@gmail.com>
2025-08-13 22:02:10 +0530

    Time: 38 ms (70.68%) | Memory: 53.6 MB (28.57%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -1,10 +1,16 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
 Array.prototype.last = function() {
-    return this.length ? this[this.length -1] :-1;
+    return this.length ? this[this.length -1]:-1
     
 };
 
-let num =[null,{},3];
-console.log(num.last());
-
-let num2=[];
-console.log(num2.last());
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+ let=num1 = [null, {}, 3]
+ num1.last();
+ let=num2=[]
+ num2.last();
